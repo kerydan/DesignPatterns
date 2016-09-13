7b97f6671446451e63f7a722d55cd51cb87021ac
Andrey Kazlitin <akazlitin@astrata.eu>
2016-09-13 10:02:48 +0200

    State pattern implemented

--- src/C++/Behavioral/State.cpp
+++ src/C++/Behavioral/State.cpp
@@ -1,11 +1,21 @@
-//
 #include <stdio.h>
 #include <iostream>
 
+class PowerManager
+{
+public:
+	PowerManager();
+	void GoSuspendPower();
+	void GoFullPower();
+	void GoLowPower();
 
+private:
+	friend class PowerState;
+	void ChangeState(PowerState* _state) { state = _state; };
+	PowerState* state;
+};
 
-
-class PowerState 
+class PowerState
 {
 public:
 	static PowerState* GetInstance();
@@ -18,19 +28,17 @@ protected:
 	void ChangeState(PowerManager* man, PowerState* state) { man->ChangeState(state); };
 };
 
-
-class LowPowerState : public PowerState 
+class LowPowerState : public PowerState
 {
 public:
-	static PowerState* GetInstance() 
+	static PowerState* GetInstance()
 	{
 		static LowPowerState state;
 		return &state;
 	};
-	void GoSuspendPower() { ChangeState }
+	void GoSuspendPower(PowerManager *man);
+	void GoFullPower(PowerManager *man);
 };
-
-
 
 class FullPowerState : public PowerState
 {
@@ -50,48 +58,55 @@ public:
 		static SuspendPowerState state;
 		return &state;
 	};
+	void GoFullPower(PowerManager *man);
 };
 
-
-
-class PowerManager 
-{
-public:
-	PowerManager();
-	void GoSuspendPower(PowerManager *man) 
-	{ 
-		// do whatever needed prior going to the suspend mode
-		state->GoSuspendPower(this); 
-	};
-	void GoFullPower()
-	{
-		// do whatever needed prior going to the suspend mode
-		state->GoFullPower(this);
-	};
-
-	void GoLowPower()
-	{
-		// do whatever needed prior going to the suspend mode
-		state->GoLowPower(this);
-	};
-
-private:
-	friend class PowerState;
-	void ChangeState(PowerState* _state) { state = _state; };
-	PowerState* state;
-};
 
 PowerManager::PowerManager() 
-{
-	state = LowPowerState::GetInstance();
-
+{ 
+	state = LowPowerState::GetInstance(); 
 }
 
+void PowerManager::GoSuspendPower()
+{
+	// do whatever needed prior going to the suspend mode
+	state->GoSuspendPower(this);
+};
 
+void PowerManager::GoFullPower()
+{
+	// do whatever needed prior going to the suspend mode
+	state->GoFullPower(this);
+};
+
+void PowerManager::GoLowPower()
+{
+	// do whatever needed prior going to the suspend mode
+	state->GoLowPower(this);
+};
+
+void LowPowerState::GoSuspendPower(PowerManager *man)
+{
+	// save configuration to RAM
+	ChangeState(man, SuspendPowerState::GetInstance());
+}
+
+void LowPowerState::GoFullPower(PowerManager *man)
+{
+	// start modules
+	ChangeState(man, FullPowerState::GetInstance());
+}
+
+void SuspendPowerState::GoFullPower(PowerManager *man)
+{
+	// load configuration from RAM
+	// start modules
+	ChangeState(man, FullPowerState::GetInstance());
+}
 
 int main()
 {
-	
-
+	PowerManager power_man;
+	power_man.GoFullPower();
 	return 0;
 }
